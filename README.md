2e6b98dbf7de53d41c3f1c2943d82c4e264caff3
이태훈 <rhymint@gmail.com>
2025-02-11 14:03:11 +0900

    chore: 프로젝트 기본 세팅

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ This Turborepo includes the following packages/apps:
 
 - `docs`: a [Next.js](https://nextjs.org/) app
 - `web`: another [Next.js](https://nextjs.org/) app
-- `@repo/ui`: a stub React component library shared by both `web` and `docs` applications
-- `@repo/eslint-config`: `eslint` configurations (includes `eslint-config-next` and `eslint-config-prettier`)
-- `@repo/typescript-config`: `tsconfig.json`s used throughout the monorepo
+- `@pin-to-gather/ui`: a stub React component library shared by both `web` and `docs` applications
+- `@pin-to-gather/eslint-config`: `eslint` configurations (includes `eslint-config-next` and `eslint-config-prettier`)
+- `@pin-to-gather/typescript-config`: `tsconfig.json`s used throughout the monorepo
 
 Each package/app is 100% [TypeScript](https://www.typescriptlang.org/).
 
---
+++ apps/web/app/components/Map/Map.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Coordinates, NaverMap } from "../../../types/map";
+import Script from "next/script";
+
+interface Props {
+  loc: Coordinates;
+}
+
+export function Map({ loc }: Props) {
+  const mapRef = useRef<NaverMap>(null);
+
+  const [currentLoc, setCurrentLoc] = useState<Coordinates>();
+  const current = navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude, position.coords.longitude);
+  });
+
+  const initializeMap = useCallback((loc: Coordinates) => {
+    const mapOptions = {
+      center: new window.naver.maps.LatLng(loc),
+      zoom: 9,
+    };
+    const map = new window.naver.maps.Map("map", mapOptions);
+    mapRef.current = map;
+  }, [loc]);
+
+  const handleClick = () => {
+    mapRef.current?.setCenter(new window.naver.maps.LatLng(37.42829747263545, 126.76620435615891));
+  };
+
+  return (
+    <>
+    <Script
+        strategy="afterInteractive"
+        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=myuj2bw9tf`}
+        onLoad={() => initializeMap(loc)}
+      ></Script>
+      <div id="map" style={{ width: "80vw", height: "80vh" }} />
+      <button onClick={handleClick}>click</button>
+    </>
+  );
+}
