f9213c4752f46cbd841ede49f221565b7c03b5e4
이태훈 <rhymint@gmail.com>
2025-02-18 14:55:29 +0900

    feat: boardUuid로 이동하도록 변경

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { Map } from "../components/Map/Map";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const boardUuid = "1";
-  const userId = crypto.randomUUID();
+  const [boardUuid, setBoardUuid] = useState<string>("");
+  const router = useRouter();
 
   return (
      <>
-      <Map boardUuid={boardUuid} userId={userId} />
+      <input value={boardUuid} onChange={(e) => setBoardUuid(e.target.value)} />
+      <button onClick={() => {
+        router.push(`/${boardUuid}`);
+      }}>
+        이동
+      </button>
     </>
   );
 }
