eafcff5b4af1d73b045ce2d60758df8ec18b5f41
이태훈 <rhymint@gmail.com>
2025-02-19 17:13:15 +0900

    feat: http 모듈에서의 타임아웃과 error 처리 로직 추가

--- apps/web/utils/http.ts
+++ apps/web/utils/http.ts
@@ -1,17 +1,30 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, {AxiosRequestConfig} from 'axios';
+
+const DEFAULT_TIMEOUT = 5000;
+
+const axiosInstance = axios.create({
+  timeout: DEFAULT_TIMEOUT,
+});
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    //TODO: 에러 로깅 또는 처리
+    return Promise.reject(error);
+  },
+);
 
 export const http = {
   get: async function get<Response = unknown>(url: string, options: AxiosRequestConfig = {}) {
-    const res = await axios.get<Response>(url, options);
+    const res = await axiosInstance.get<Response>(url, options);
     return res.data;
   },
-  post: async function post<Request = any, Response = unknown>(url: string, data?: Request) {
-    const res = await axios.post<Response>(url, {...data});
+  post: async function post<Request extends Record<string, unknown>, Response = unknown>(url: string, data?: Request) {
+    const res = await axiosInstance.post<Response>(url, data);
     return res.data;
   },
-  put: async function put<Request = any, Response = unknown>(url: string, data?: Request) {
-    const res = await axios.put<Response>(url, {...data});
+  put: async function put<Request extends Record<string, unknown>, Response = unknown>(url: string, data?: Request) {
+    const res = await axiosInstance.put<Response>(url, data);
     return res.data;
   },
 };
