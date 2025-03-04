bc89dd64541e056e3a9e701d1821745fccba70f0
이태훈 <rhymint@gmail.com>
2025-03-04 12:56:38 +0900

    fix: localSearch API를 백엔드에서 호출

--- apps/web/api/localSearch.ts
+++ apps/web/api/localSearch.ts
@@ -1,13 +1,14 @@
 import {LocalItem} from '@type/model';
 import {http} from '@utils/http';
+import {API_BASE_URL} from './config';
 
-type Sort = 'random' | 'comment';
+// type Sort = 'random' | 'comment';
 
 export interface LocalSearchRequest {
   query: string;
-  display?: number;
-  start?: number;
-  sort?: Sort;
+  // display?: number;
+  // start?: number;
+  // sort?: Sort;
 }
 
 export interface LocalSearchResponse {
@@ -19,24 +20,28 @@ export interface LocalSearchResponse {
 }
 
 export const getLocalSearch = ({params}: {params: LocalSearchRequest}) => {
-  const searchParams = new URLSearchParams({
-    query: params.query,
-    display: params.display?.toString() ?? '5',
-    start: params.start?.toString() ?? '1',
-    sort: params.sort ?? 'random',
-  });
+  // const searchParams = new URLSearchParams({
+  //   query: params.query,
+  //   display: params.display?.toString() ?? '5',
+  //   start: params.start?.toString() ?? '1',
+  //   sort: params.sort ?? 'random',
+  // });
 
-  const clientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
-  const clientSecret = process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET;
+  // const clientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
+  // const clientSecret = process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET;
 
-  if (!clientId || !clientSecret) {
-    throw new Error('Naver API 인증 정보가 설정되지 않았습니다.');
-  }
+  // if (!clientId || !clientSecret) {
+  //   throw new Error('Naver API 인증 정보가 설정되지 않았습니다.');
+  // }
 
-  return http.get<LocalSearchResponse>(`/naver-api/v1/search/local.json?${searchParams.toString()}`, {
-    headers: {
-      'X-Naver-Client-Id': clientId,
-      'X-Naver-Client-Secret': clientSecret,
-    },
+  // return http.get<LocalSearchResponse>(`/naver-api/v1/search/local.json?${searchParams.toString()}`, {
+  //   headers: {
+  //     'X-Naver-Client-Id': clientId,
+  //     'X-Naver-Client-Secret': clientSecret,
+  //   },
+  // });
+
+  return http.get<LocalSearchResponse>(`${API_BASE_URL}/naver/local-search`, {
+    params,
   });
 };
